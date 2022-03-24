0bf6b42fa1691162ce42b6ba807e27623e9bc7a0
Kutorglo Prince Wells <87288156+PrinceKutt1@users.noreply.github.com>
2022-03-24 22:31:19 +1100

    writing test for contact routes

---
+++ test/contact.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Appartment = require('../models/appartment');
+process.env.NODE_ENV = 'MONGO_URI';
+//Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const index = require('../index');
+const appartment = require("../models/contact");
+const should = chai.should();
